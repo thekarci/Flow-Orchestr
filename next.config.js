036c359b2d8eaebeb53be80806cedef0113d2d41
thekarci <okarci88@gmail.com>
2025-11-15 15:38:12 +0300

    next.config.js gÃ¼ncellendi

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    eslint: {
+        ignoreDuringBuilds: true, // Build sırasında ESLint'i atla
+      },
+
+}
 
 module.exports = nextConfig
 
